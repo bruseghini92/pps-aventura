eb9afa005f2370bd716bdd035588b7aeff0dad7d
Bruseghini92 <bruseghini92.ab@gmail.com>
2018-05-22 19:05:53 -0300

    #1 creando modelo Aventuras

--- pps_aventura/pps_aventura/settings.py
+++ pps_aventura/pps_aventura/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'aventuras.apps.AventurasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pps_aventura/pps_aventura/urls.py
+++ pps_aventura/pps_aventura/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from . import templates
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('aventuras/', include('aventuras.urls')),
 ]
