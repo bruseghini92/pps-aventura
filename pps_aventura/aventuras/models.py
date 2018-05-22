23891091ce5ed397f4d9228325e6ddf96d8770ff
Bruseghini92 <bruseghini92.ab@gmail.com>
2018-05-22 19:36:14 -0300

    #1 Corregido modelo eventos -Creadas migraciones -Incorporado el modelo al panel administrativo

--- pps_aventura/aventuras/models.py
+++ pps_aventura/aventuras/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Aventuras (models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fechaInicio = models.DateTimeField()
+    fechaInscripcion = models.DateTimeField()
