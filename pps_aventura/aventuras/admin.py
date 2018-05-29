9e526a121deee3e7f2d8c84044bd1cdb2e340db9
leansabio <leansabio@gmail.com>
2018-05-29 11:04:33 -0300

    Creado modelo evento

--- pps_aventura/aventuras/admin.py
+++ pps_aventura/aventuras/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+#from django.contrib.gis.db import models as modelgis
+from django.db import models
+from .models import Evento
+
+#from mapwidgets.widgets import GooglePointFieldWidget
 
 # Register your models here.
+
+admin.site.register(Evento)
+
+"""
+class CityAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        modelgis.PointField: {"widget": GooglePointFieldWidget}
+    }
+"""
